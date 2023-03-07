114cdd289354930b22bb8df751e9a8c96cf9083f
Buidlor <89695544+Buidlor@users.noreply.github.com>
2023-03-07 23:00:16 +0100

    backend paths for metamask connect

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const Moralis = require('moralis').default;
 const {EvmChain} = require('@moralisweb3/common-evm-utils');
 require('dotenv').config();
@@ -10,13 +11,24 @@ const port = 3000;
 const chain = EvmChain.ARBITRUM;
 const address = process.env.ADDRESS;
 
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(cors({
+    origin: process.env.REACT_URL,
+    credentials: true,
+}));
+
+
 const config = {
     domain: process.env.APP_DOMAIN,
     statement: 'Please sign this message to confirm your identity.',
-    uri: process.env.REACT_URI,
+    uri: process.env.REACT_URL,
     timeout: 60,
 };
 
+// request message to be signed by client
 app.post('/request-message', async (req, res) => {
     const {address, chain, network} = req.body;
     try{
@@ -27,19 +39,69 @@ app.post('/request-message', async (req, res) => {
             ...config,
         });
         res.status(200).json(message);
-    }catch(error) {
+    }   catch(error) {
         res.status(400).json({error: error.message});
         console.error(error);
     }
 });
 
+app.post('/verify', async (req, res) => {
+    try {
+        const {message, signature} = req.body;
+
+        const { address, profileId } = (
+            await Moralis.Auth.verify({
+                message,
+                signature,
+                networkType: 'evm',
+            })
+
+        ).raw;
+
+        const user = { address, profileId, signature };
+
+        //create JWT token
+        const token = jwt.sign(user, process.env.AUTH_SECRET);
+
+        //set JWT cookie
+        res.cookie('jwt', token, {
+            httpOnly: true,
+        });
+        res.status(200).json(user);
+    } catch(error) {
+        res.status(400).json({error: error.message});
+        console.error(error);
+    }
+});
+
+app.get('/authenticate', async (req, res) => {
+    const token = req.cookies.jwt;
+    if (!token) return res.status(403); //if user has no jwt token, return 403 forbidden
+    try{
+        const data = jwt.verify(token, process.env.AUTH_SECRET);
+        res.json(data);
+    } catch {
+        return res.sendStatus(403);
+    }
+});
+
+app.get('/logout', async (req, res) => {
+    try{
+        res.clearCookie('jwt');
+        return res.sendStatus(200);
+    } catch {
+        return res.sendStatus(403);
+    }
+});
+    
 
 app.get('/', (req, res) => res.send('hello world'));
+
 const startServer = async () => {
     await Moralis.start({
         apiKey: process.env.MORALIS_API_KEY,
-    })
-}
+    });
+};
 
 
 
